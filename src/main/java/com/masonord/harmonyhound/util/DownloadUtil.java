841705d690f446d467b8a086b956fd7f1e28296c
masonord <trotf233@gmail.com>
2024-04-22 01:02:15 +0300

    Impl a function to convert audio file to wav format

---
+++ src/main/java/com/masonord/harmonyhound/util/DownloadUtil.java
@@ -0,0 +1,129 @@
+package com.masonord.harmonyhound.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.masonord.harmonyhound.response.FilePathResponse;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.io.*;
+
+@Component
+public class DownloadUtil {
+    private final ObjectMapper objectMapper;
+    private final String botToken;
+
+    public DownloadUtil(@Value("${telegram.bot-token}") String botToken) {
+        this.objectMapper = new ObjectMapper();
+        this.botToken = botToken;
+    }
+
+    public FilePathResponse download(String fieldId, String chatId) {
+        FilePathResponse filePathResponse = getFilePath(fieldId);
+        String destinationToDownload = "downloaded-media/chat_" + chatId + "/"
+                + filePathResponse.getResult().getFile_path().split("/")[1];
+
+        createNewFolder("downloaded-media/chat_" + chatId);
+
+        String url = "https://api.telegram.org/file/bot" + botToken + "/" + filePathResponse.getResult().getFile_path();
+        HttpGet httpGet = new HttpGet(url);
+
+        createNewFile(destinationToDownload);
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+             CloseableHttpResponse response = httpClient.execute(httpGet)) {
+
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+                try (InputStream inputStream = entity.getContent();
+                        FileOutputStream fileOutputStream = new FileOutputStream(destinationToDownload)) {
+                    int read;
+                    byte[] buffer = new byte[4096];
+                    while ((read = inputStream.read(buffer)) != -1) {
+                        fileOutputStream.write(buffer, 0, read);
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            // TODO: logging
+        }
+
+        return filePathResponse;
+    }
+
+    /*
+    *
+    *
+    *
+    * */
+
+    private FilePathResponse getFilePath(String fieldId) {
+        String url = "https://api.telegram.org/bot" + botToken + "/getFile?file_id=" + fieldId;
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet(url);
+        StringBuilder result = new StringBuilder();
+        try {
+            HttpResponse response = client.execute(request);
+            BufferedReader rd = new BufferedReader(new InputStreamReader(
+                    response.getEntity().getContent()
+            ));
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+        }catch(IOException e) {
+            // TODO: logging
+            System.out.println(e.getMessage());
+        }
+
+        FilePathResponse response = null;
+        try {
+            response =  objectMapper.readValue(result.toString(), FilePathResponse.class);
+        }catch(JsonProcessingException e) {
+            // TODO: logging
+            System.out.println(e.getMessage());
+        }
+
+        return response;
+    }
+
+    private void createNewFile(String destination) {
+        try {
+            File file = new File(destination);
+            boolean fileCreated = file.createNewFile();
+            if (!fileCreated) {
+                throw new IOException("Unable to create a file at specified path");
+            }
+        }catch (IOException e) {
+            // TODO: logging
+        }
+    }
+
+    private void createNewFolder(String destination) {
+        try {
+            File file = new File(destination);
+            boolean folderCreated = true;
+
+            if (!file.exists()) {
+                folderCreated = file.mkdirs();
+            }
+
+            if (!folderCreated) {
+                throw new IOException("Unable to create a folder at specified path");
+            }
+        }catch(IOException e) {
+            // TODO: logging
+        }
+    }
+
+
+}
--- src/main/java/com/masonord/harmonyhound/util/MediaUtil.java
+++ src/main/java/com/masonord/harmonyhound/util/MediaUtil.java
@@ -1,104 +1,36 @@
 package com.masonord.harmonyhound.util;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.masonord.harmonyhound.response.FilePathResponse;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 
 @Component
 public class MediaUtil {
-    private final ObjectMapper objectMapper;
-    private final String botToken;
 
-    public MediaUtil(@Value("${telegram.bot-token}") String botToken) {
-        this.objectMapper = new ObjectMapper();
-        this.botToken = botToken;
+    private final String ffmpegPath;
+    private final String ffprobePath;
+
+    public MediaUtil(@Value("${ffmpeg.path}") String ffmpegPath,
+                     @Value("${ffprobe.path}") String ffprobePath) {
+        this.ffmpegPath = ffmpegPath;
+        this.ffprobePath = ffprobePath;
     }
 
-    public void download(String fieldId) {
-        FilePathResponse filePathResponse = getFilePath(fieldId);
-        String url = "https://api.telegram.org/file/bot" + botToken + "/" + filePathResponse.getResult().getFile_path();
-        String destination = "downloaded-media" + "/" + filePathResponse.getResult().getFile_path().split("/")[1];
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet(url);
-
+    public void convertAudioToWavFormat(String chatId, FilePathResponse filePathResponse) {
+        String fileName = filePathResponse.getResult().getFile_path().split("/")[1];
+        String inFilename ="downloaded-media/chat_" + chatId + "/" + fileName;
+        String outFilename ="downloaded-media/chat_" + chatId + "/" + fileName.split("\\.(?=[^\\.]+$)")[0] + ".wav";
+        String cmd = "ffmpeg -i " + inFilename + " -ar 8000 -ac 1 -vn " + outFilename;
         try {
-            File file = new File(destination);
-            boolean fileCreated = file.createNewFile();
-            if (!fileCreated) {
-                throw new IOException("Unable to create a file at specified path");
-            }
-        }catch (IOException e) {
+            Process p = Runtime.getRuntime().exec(cmd);
+            p.waitFor();
+            File file = new File(inFilename);
+            boolean done = file.delete();
+        }catch (IOException | InterruptedException e) {
             // TODO: logging
         }
-
-        try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
-            HttpEntity entity = response.getEntity();
-            if (entity != null) {
-                try (InputStream inputStream = entity.getContent();
-                        FileOutputStream fileOutputStream = new FileOutputStream(destination)) {
-                    int read;
-                    byte[] buffer = new byte[4096];
-                    while ((read = inputStream.read(buffer)) != -1) {
-                        fileOutputStream.write(buffer, 0, read);
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                httpClient.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    /*
-    *
-    *
-    *
-    * */
-
-    private FilePathResponse getFilePath(String fieldId) {
-        String url = "https://api.telegram.org/bot" + botToken + "/getFile?file_id=" + fieldId;
-        HttpClient client = HttpClientBuilder.create().build();
-        HttpGet request = new HttpGet(url);
-        StringBuilder result = new StringBuilder();
-        try {
-            HttpResponse response = client.execute(request);
-            BufferedReader rd = new BufferedReader(new InputStreamReader(
-                    response.getEntity().getContent()
-            ));
-            String line = "";
-            while ((line = rd.readLine()) != null) {
-                result.append(line);
-            }
-        }catch(IOException e) {
-            // TODO: logging
-            System.out.println(e.getMessage());
-        }
-
-        FilePathResponse response = null;
-        try {
-            response =  objectMapper.readValue(result.toString(), FilePathResponse.class);
-        }catch(JsonProcessingException e) {
-            // TODO: logging
-            System.out.println(e.getMessage());
-        }
-
-        return response;
     }
 }
