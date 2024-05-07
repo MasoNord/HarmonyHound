9e0fdedd82e037617e0c8c1901ce862134da7ce1
masonord <trotf233@gmail.com>
2024-05-07 23:44:58 +0300

    Set config properties for mongodb

--- src/main/java/com/masonord/harmonyhound/HarmonyHoundApplication.java
+++ src/main/java/com/masonord/harmonyhound/HarmonyHoundApplication.java
@@ -3,8 +3,9 @@ package com.masonord.harmonyhound;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class})
 public class HarmonyHoundApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HarmonyHoundApplication.class, args);
