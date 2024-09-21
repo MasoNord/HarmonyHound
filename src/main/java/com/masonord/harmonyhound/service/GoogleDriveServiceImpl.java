c2cd1c5a2525c4c431660a2ec775e9f994597131
masonord <trotf233@gmail.com>
2024-09-22 02:02:14 +0300

    add the ability to share an upload file with others

--- src/main/java/com/masonord/harmonyhound/service/GoogleDriveServiceImpl.java
+++ src/main/java/com/masonord/harmonyhound/service/GoogleDriveServiceImpl.java
@@ -17,12 +17,12 @@ import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.Permission;
 import com.masonord.harmonyhound.response.FilePathResponse;
+import com.masonord.harmonyhound.util.FileSystemUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.io.*;
 import java.security.GeneralSecurityException;
-import java.util.Collections;
-
-import java.util.Set;
+import java.util.*;
 
 @Service
 public class GoogleDriveServiceImpl implements GoogleDriveService {
@@ -31,6 +31,42 @@ public class GoogleDriveServiceImpl implements GoogleDriveService {
     private static final String TOKENS_DIRECTORY_PATH = "/home/masonord/credentials";
     private static final Set<String> SCOPES = Collections.singleton(DriveScopes.DRIVE_FILE);
     private static final String CREDENTIALS_FILE_PATH = "credentials.json";
+
+    @Autowired
+    private FileSystemUtil fileSystemUtil;
+
+    @Override
+    public String uploadFile(FilePathResponse filePathResponse, String chatId) throws GeneralSecurityException, IOException {
+        NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredential(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+        File fileMetadata = new File();
+        fileMetadata.setName(filePathResponse.getResult().getFile_path().split("/")[1]);
+        java.io.File filePath = new java.io.File("downloaded-media/chat_" + chatId + "/"
+                + filePathResponse.getResult().getFile_path().split("/")[1]);
+        FileContent mediaContent = new FileContent("audio/ogg", filePath);
+        File fileLink = null;
+        try {
+            File file = service.files().create(fileMetadata, mediaContent)
+                    .setFields("id")
+                    .execute();
+            Permission permission = new Permission();
+            permission.setType("anyone");
+            permission.setRole("reader");
+
+            service.permissions().create(file.getId(), permission).execute();
+            fileLink = service.files().get(file.getId())
+                    .setFields("id, webViewLink, webContentLink")
+                    .execute();
+        }catch (GoogleJsonResponseException e) {
+            System.out.println("Unable to upload file: " + e.getDetails());
+        }
+
+        fileSystemUtil.deleteFile("downloaded-media/chat_" + chatId + "/"
+                + filePathResponse.getResult().getFile_path().split("/")[1]);
+        return fileLink.getWebViewLink();
+    }
 
     private Credential getCredential(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
         InputStream in = GoogleDriveServiceImpl.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
@@ -45,41 +81,9 @@ public class GoogleDriveServiceImpl implements GoogleDriveService {
                 .setAccessType("offline")
                 .build();
 
-
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8080).build();
-
         Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
         return credential;
     }
 
-    private String uploadFile(FilePathResponse filePathResponse, String chatId) throws GeneralSecurityException, IOException {
-        NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredential(HTTP_TRANSPORT))
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-        File fileMetadata = new File();
-        fileMetadata.setName(filePathResponse.getResult().getFile_path().split("/")[1]);
-        java.io.File filePath = new java.io.File("downloaded-media/chat_" + chatId + "/"
-                + filePathResponse.getResult().getFile_path().split("/")[1]);
-        FileContent mediaContent = new FileContent("audio/ogg", filePath);
-        Permission permission = new Permission().setType("anyone").setRole("reader");
-        String fileId = null;
-
-        try {
-            File file = service.files().create(fileMetadata, mediaContent)
-                    .setFields("id")
-                    .execute();
-            System.out.println("File ID: " + file.getId());
-            System.out.println("File link: " + file.getWebViewLink());
-            fileId = file.getId();
-        }catch (GoogleJsonResponseException e) {
-            System.out.println("Unable to upload file: " + e.getDetails());
-        }
-        return fileId;
-    }
-
-    @Override
-    public void test(FilePathResponse filePathResponse, String chatId) throws IOException, GeneralSecurityException {
-        String fileId = uploadFile(filePathResponse, chatId);
-    }
 }
--- src/main/java/com/masonord/harmonyhound/service/RecognizeMediaService.java
+++ src/main/java/com/masonord/harmonyhound/service/RecognizeMediaService.java
@@ -1,7 +1,7 @@
 package com.masonord.harmonyhound.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.masonord.harmonyhound.response.FilePathResponse;
+import com.google.gson.Gson;
+import com.masonord.harmonyhound.response.RecognizedSongResponse;
 import com.masonord.harmonyhound.util.FileSystemUtil;
 import com.masonord.harmonyhound.util.MediaUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +20,7 @@ public class RecognizeMediaService {
     private final String apikey;
     private final String url;
     private final String rapidapi;
-    private final ObjectMapper objectMapper;
+    private final Gson gson;
 
     @Autowired
     private MediaUtil mediaUtil;
@@ -34,18 +34,20 @@ public class RecognizeMediaService {
         this.apikey = apikey;
         this.url = url;
         this.rapidapi = rapidapi;
-        this.objectMapper = new ObjectMapper();
+        this.gson = new Gson();
     }
 
-    public String recognizeAudio(String chatId, FilePathResponse filePathResponse) throws IOException, InterruptedException, URISyntaxException {
+    public String recognizeAudio(String fileLink) throws IOException, InterruptedException, URISyntaxException {
+        URI uri = new URI("https://shazam-song-recognition-api.p.rapidapi.com/recognize/url?url=" + fileLink);
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://shazam-song-recognition-api.p.rapidapi.com/recognize/url?url=https://drive.google.com/file/d/1PpRMf-N864zVwIePXDprDWrh5lmfdN9O/view?usp=drive_link"))
+                .uri(uri)
                 .header("x-rapidapi-key", "7ef5347179msh324744296b9e5e8p1e55e2jsnbc0c396235f4")
                 .header("x-rapidapi-host", "shazam-song-recognition-api.p.rapidapi.com")
                 .method("GET", HttpRequest.BodyPublishers.noBody())
                 .build();
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
+        RecognizedSongResponse result = gson.fromJson(response.body(), RecognizedSongResponse.class);
         return response.body();
     }
 }
